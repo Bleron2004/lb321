882c5dd3404895544fc0c320341a7c55987a30dd
switzerchees <patrickmichel96@yahoo.de>
2024-05-02 15:09:33 +0200

    Add simple message ping-pong as example, update docker-compose & Dockerfile

--- server/websocketserver.js
+++ server/websocketserver.js
@@ -1,5 +1,7 @@
 const WebSocket = require("ws");
 
+const clients = [];
+
 // Intiiate the websocket server
 const initializeWebsocketServer = (server) => {
   const websocketServer = new WebSocket.Server({ server });
@@ -13,9 +15,45 @@ const onConnection = (ws) => {
 };
 
 // If a new message is received, the onMessage function is called
-const onMessage = (ws, message) => {
-  console.log("Message received: " + message);
-  ws.send("Hello, you sent -> " + message);
+const onMessage = (ws, messageBuffer) => {
+  const messageString = messageBuffer.toString();
+  const message = JSON.parse(messageString);
+  console.log("Received message: " + messageString);
+  switch (message.type) {
+    case "user": {
+      clients.push({ ws, user: message.user });
+      const usersMessage = {
+        type: "users",
+        users: clients.map((client) => client.user),
+      };
+      clients.forEach((client) => {
+        client.ws.send(JSON.stringify(usersMessage));
+      });
+      ws.on("close", () => onDisconnect(ws));
+      break;
+    }
+    case "message": {
+      clients.forEach((client) => {
+        client.ws.send(messageString);
+      });
+      break;
+    }
+    default: {
+      console.log("Unknown message type: " + message.type);
+    }
+  }
+};
+
+const onDisconnect = (ws) => {
+  const index = clients.findIndex((client) => client.ws === ws);
+  clients.splice(index, 1);
+  const usersMessage = {
+    type: "users",
+    users: clients.map((client) => client.user),
+  };
+  clients.forEach((client) => {
+    client.ws.send(JSON.stringify(usersMessage));
+  });
 };
 
 module.exports = { initializeWebsocketServer };
